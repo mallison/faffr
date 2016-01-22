76c29ba087fb6cdee3eb15b985ff63442ed6bef4
Mark Allison <mark@digital-impacts.org>
2016-01-22 11:13:16 +0000

    Refactor Slot

--- js/components/Slot.jsx
+++ js/components/Slot.jsx
@@ -11,37 +11,60 @@ export default class Slot extends React.Component {
     return (
       <div>
         {this.props.isEditable ?
-         <TaskSwitcher
-         task={this.props.task}
-         start={this.props.start}
-         withSave={true}
-         onStartTask={this.props.onUpdateSlot}
-         />
-         :
-         <div>
-         <p>
-         {this.props.start.toLocaleTimeString()}
-         {' '}
-         {this.props.task}
-         {' '}
-         <button
-         onClick={this.props.onClickEdit}
-         >
-         Edit
-         </button>
-         </p>
-         </div>
+         this._renderTaskAndTimeEditor() :
+         this._renderTaskAndTime()
          }
-         {this.props.showNote ?
-         <textarea
-                 placeholder="Add note"
-                ref={t => this._note = t}
-                rows={4}
-                cols={70}
-                value={this.props.note}
-                onChange={e => this.props.onNoteChange(e.target.value)}
-        /> : null }
+        {this.props.showNote ?
+         this._renderNoteEditor() :
+         this._renderNote()
+         }
       </div>
     );
+  }
+
+  _renderTaskAndTimeEditor() {
+    return (
+      <TaskSwitcher
+              task={this.props.task}
+              start={this.props.start}
+              withSave={true}
+              onStartTask={this.props.onUpdateSlot}
+      />
+    );
+  }
+
+  _renderTaskAndTime() {
+    return (
+      <div>
+        <p>
+          {this.props.start.toLocaleTimeString()}
+          {' '}
+          {this.props.task}
+          {' '}
+          <button
+                  onClick={this.props.onClickEdit}
+                  >
+            Edit
+          </button>
+        </p>
+      </div>
+    );
+  }
+
+  _renderNoteEditor() {
+    return (
+      <textarea
+              placeholder="Add note"
+              ref={t => this._note = t}
+              rows={4}
+              cols={70}
+              value={this.props.note}
+              onChange={e => this.props.onNoteChange(e.target.value)}
+      />
+    );
+  }
+
+  _renderNote() {
+    return <p>{this.props.note}</p>;
   }
 }
