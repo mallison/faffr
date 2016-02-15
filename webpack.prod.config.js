1759a90607a0c087585cee5ecfe72042d9358535
Mark Allison <mark@digital-impacts.org>
2016-02-15 21:15:21 +0000

    Fix webpack's production config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -33,7 +33,18 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
+    // removes a lot of debugging code in React
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }}),
+
+    // minifies your code
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
   ],
   resolve: {
     fallback: path.join(__dirname, 'node_modules'),
