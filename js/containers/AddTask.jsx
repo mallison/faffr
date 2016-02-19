9fa317abf47a84d3f4efea5127dfdff570220ce9
Mark Allison <mark@digital-impacts.org>
2016-02-19 14:04:39 +0000

    Require new task has name before adding

--- js/containers/AddTask.jsx
+++ js/containers/AddTask.jsx
@@ -19,9 +19,10 @@ let AddTask = ({ placeholder, onAddTask }) => {
               className="btn btn-primary"
               aria-label="Add task"
               onClick={() => {
-                       onAddTask(input.value);
-                       input.value = '';
-                       }}>
+                       if (input.value) {
+                         onAddTask(input.value);
+                         input.value = '';
+                       }}}>
         <span className="sr-only">Add Task</span>
         <span aria-hidden="true" className="glyphicon glyphicon-plus"></span>
       </button>
